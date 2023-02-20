d86396ada880b61fac152315ffd2c438b221f3c7
agilarasu <agilarasusaravanan@gmail.com>
2023-02-20 11:44:03 +0530

    Can show svg based on conditions

--- src/components/box-data.js
+++ src/components/box-data.js
@@ -1,5 +1,4 @@
 import '../App.css';
-import logosvg from '../Images/Weather-SVG/02d.svg'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -10,7 +9,9 @@ function BoxData() {
   const [windspeed, setWindspeed] = useState(null);
   const [rainrate, setRainrate] = useState(null);
   const [uv, setUv] = useState(null);
-  const [error, setError] = useState(null);
+  // const [error, setError] = useState(null);
+  const [icon, setIcon] = useState(null);
+  const [desc, setDesc] = useState(null);
     
   useEffect(() => {
     axios.get('http://192.168.29.79:8000/process/api')
@@ -21,52 +22,50 @@ function BoxData() {
         setWindspeed(response.data.windspeed);
         setRainrate(response.data.rainrate);
         setUv(response.data.uv);
-        
+        setIcon(response.data.icon);
+        setDesc(response.data.desc);
       })
-      .catch(error => {
-        setError(error);
-      });
+      // .catch(error => {
+      //   setError(console.log(error));
+      // });
   }, []);
-  
+  const imageUrl = `./Weather-SVG/${icon}.svg`;
     return(
       <div className="box">
-        <p>{error}</p>
         <div className="box1">
-          <div className='logo'>
-            
-            <img
-              src={logosvg} alt="logo" className="image" />
-            <h4>Scattered clouds</h4>
+          <div className='logo1'>
+            <img src={imageUrl} alt="logo" className="image" />
+            <h4>{desc}</h4>
           </div>
           <div className="text">
+            <span>{tempin}°C</span>
             <h4>Inside Temperature</h4>
-            <span>{tempin}°F</span>
           </div>
           <div className="text">
+            <span>{tempout}°C</span>
             <h4>Outside Temperature</h4>
-            <span>{tempout}°F</span>
           </div>
         </div>
 
         <div className="container1">
           <div className="row">
             <div className="col1">
-              <h3>Humidity</h3>
               <span>{humidity}%</span>
+              <h3>Humidity</h3>
             </div>
             <div className="col1">
-              <h3>Wind</h3>
               <span>{windspeed} Miles/Hr</span>
+              <h3>Wind Speed</h3>
             </div>
           </div>
           <div className="row">
             <div className="col1">
-              <h3>Rain Rate</h3>
               <span>{rainrate} mm</span>
+              <h3>Rain Rate</h3>
             </div>
             <div className="col1">
-              <h3>UV Index</h3>
               <span>{uv}</span>
+              <h3>UV Index</h3>
             </div>
           </div>
         </div>
